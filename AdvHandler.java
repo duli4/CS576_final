3ab1d395532053a2adc07a3e8e129ba5e41ad868
duli4 <liangdu@usc.edu>
2022-05-05 00:56:05 +0800

    finished data2

--- AdvHandler.java
+++ AdvHandler.java
@@ -1,4 +1,8 @@
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,8 +12,17 @@ import java.io.InputStream;
 import java.lang.Math;
 import java.util.*;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
 public class AdvHandler
@@ -35,6 +48,8 @@ public class AdvHandler
   int curFrame;
   Clip clip;
   File audio;
+  AudioInputStream audioStream;
+  AudioFormat format;
   DataLine.Info info;
   BufferedImage img;
   InputStream videoStream;
@@ -43,11 +58,13 @@ public class AdvHandler
 
   public static final int FRAMERATE = 30;
 
-  public AdvHandler(String videoPath)
+  public AdvHandler(String videoPath, String audioPath)
   {
     int period = 1000 / FRAMERATE;
     videopath = videoPath;
+    audiopath = audioPath;
     // timer = new Timer(period/2, this);
+
     prev_img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
     for(int y = 0; y < HEIGHT; y++){
         for(int x = 0; x < WIDTH; x++){
@@ -55,7 +72,14 @@ public class AdvHandler
         }
     }
 
-    
+    img3 = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    for(int y = 0; y < HEIGHT; y++){
+        for(int x = 0; x < WIDTH; x++){
+            img3.setRGB(x,y,0);
+        }
+    }
+
+
     img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
     try {
         videoStream = new FileInputStream(videopath);
@@ -141,13 +165,13 @@ public class AdvHandler
               int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
               // System.out.println("pix = " + pix);
               // BufferedImage img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-          	  img.setRGB(x,y,pix);
-              Color c = new Color(prev_img.getRGB(x, y));
-              difR += Math.abs(r - c.getRed());
-              difG += Math.abs(g - c.getGreen());
-              difB += Math.abs(b - c.getBlue());
-              sum_diff = (difR + difG +difB);
-              prev_img.setRGB(x, y, pix);
+              img.setRGB(x,y,pix);
+                Color c = new Color(prev_img.getRGB(x, y));
+                difR += Math.abs(r - c.getRed());
+                difG += Math.abs(g - c.getGreen());
+                difB += Math.abs(b - c.getBlue());
+                sum_diff = (difR + difG +difB);
+                prev_img.setRGB(x, y, pix);
 
               ++ind;
           }
@@ -155,16 +179,27 @@ public class AdvHandler
 
       if (sum_diff/(HEIGHT * WIDTH) > 170)
       {
-        //System.out.println("current curFrame: " + curFrame + "  differ sum = " + sum_diff/(HEIGHT * WIDTH));
+        // System.out.println("current time: " + curFrame/30 + "  differ sum = " + sum_diff/(HEIGHT * WIDTH));
 
-        time_clip.push(curFrame);
+        time_clip.push(curFrame/30);
+            System.out.println("time list: " + time_clip);
+        for(int y = 0; y < HEIGHT; y++){
+            for(int x = 0; x < WIDTH; x++){
+              Color c = new Color(prev_img.getRGB(x, y));
+              int r = c.getRed();
+              int g = c.getGreen();
+              int b = c.getBlue();
+              int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+              // System.out.println("current time: " + curFrame/30 + "  differ sum = " + sum_diff/(HEIGHT * WIDTH));
+              img3.setRGB(x, y, pix);
+            }
+          }
       }
       difR = 0;
       difG = 0;
       difB = 0;
 
       sum_diff = 0;
-      
       curFrame++;
     }catch (IOException e) {
         e.printStackTrace();
@@ -183,21 +218,52 @@ public class AdvHandler
     for(int i =1;i<time_clip.size();i++)
     {
       // System.out.println("nonono:" + arr[i]);
-      if (arr[i] - prev <  390 && flag == 0)
+      if (arr[i] - prev <  13 && flag == 0)
       {
-        res.push(prev);
+        res.push(prev * 30);
         flag = 1;
       }
-      if(flag == 1 && arr[i] - prev >= 390)
+      if(flag == 1 && arr[i] - prev >=13)
       {
-        res.push(prev);
+        res.push(prev * 30);
         flag = 0;
       }
       prev = arr[i];
     }
-    if (flag == 1) {
-    	res.push(9000);
+    if(res.size() % 2 == 1)
+    {
+      res.push(arr[time_clip.size()-1] * 30);
     }
-    return res;
+
+
+    Stack<Integer> ret = new Stack<Integer>();
+    Integer[] arr1 = null;
+    System.out.println("array : " + res);
+    arr1 = res.toArray(new Integer[res.size()]);
+    for(int i =0;i<res.size();i += 2)
+    {
+      if (arr1[i+1] - arr1[i] < 450)
+      {
+        continue;
+      }
+      else if(arr1[i+1] - arr1[i] > 450)
+      {
+        ret.push(arr1[i]);
+        ret.push(arr1[i] + 450);
+      }
+      else{//when euqal
+        ret.push(arr1[i]);
+        ret.push(arr1[i+1]);
+      }
+    }
+    return ret;
+  }
+
+
+  public static void main(String[] args)
+  {
+      AdvHandler advHandler = new AdvHandler(args[0], args[1]);
+       Stack<Integer> result = advHandler.get_frame_list();
+        System.out.println("arr:" + result);
   }
 }
